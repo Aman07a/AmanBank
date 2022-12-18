b41ac9d862353343d4a27d481b8f9a500a642322
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 11:34:16 +0100

    Creating Login Form

--- AmanBank.Presentation/Program.cs
+++ AmanBank.Presentation/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AmanBank.Presentation
+﻿namespace AmanBank.Presentation
 {
-	internal class Program
+	class Program
 	{
-		static void Main(string[] args)
+		// Application execution starts here
+		static void Main()
 		{
+			// Display title
+			System.Console.WriteLine("************** Harsha Bank *****************");
+			System.Console.WriteLine("::Login Page::");
+
+			// Declare variables to store username and password;
+			string userName = null, password = null;
+
+			// Read userName from keyboard
+			System.Console.Write("Username: ");
+			userName = System.Console.ReadLine();
+
+			// Read password from keyboard only if username is entered
+			if (userName != "")
+			{
+				System.Console.Write("Password: ");
+				password = System.Console.ReadLine();
+			}
+
+			// Check username and password
+			if (userName == "system" && password == "manager")
+			{
+				System.Console.WriteLine("TO DO: Main menu here");
+			}
+
+			// About to exit
+			System.Console.WriteLine("Thank you! Visit again.");
+			System.Console.ReadKey();
 		}
 	}
 }
